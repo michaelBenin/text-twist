48ca89a0b2291be3642354863d058eb4741c2b2b
Michael Benin <beninmichael@gmail.com>
2012-06-05 16:19:44 -0400

    Updating changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a class project for Advanced JS @ NYU.
+https://github.com/afeld/advanced_js
+https://www.lucidchart.com/documents/view/421d-efc0-4fce684c-9f72-1b8b0af03250
--- js/text_twist.js
+++ js/text_twist.js
@@ -1,1 +1,52 @@
-// JavaScript Document
+/* 
+Text Twist
+Michael Benin 2012
+*/
+
+/* 	Begin App */
+var TextTwist = (function(w, d)
+{
+/* 	Initialize Variables */
+	
+})(window, document);
+
+/* 	End App */
+
+/* Google Analytics */
+window._gaq = _gaq || [];
+_gaq.push(['_setAccount', 'GA ID HERE']);
+_gaq.push(['_trackPageview']);
+(function(d) 
+{
+	var ga, s = d.getElementsByTagName('script')[0];
+	ga = d.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == d.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';  
+	s.parentNode.insertBefore(ga, s);
+})(document);
+/* End Google Analytics */
+
+/* Facebook API */
+(function(d)
+{
+	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement('script'); js.id = id; js.async = true;
+	js.src = "//connect.facebook.net/en_US/all.js";
+	ref.parentNode.insertBefore(js, ref);
+}(document));
+window.fbAsyncInit = function() 
+{
+    FB.init({
+      appId      : '323907284355490', //'YOUR_APP_ID', 
+      //channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', 
+      status     : true, 
+      cookie     : true, 
+      xfbml      : true,  
+	  frictionlessRequests : true
+    });
+	 
+	FB.getLoginStatus(function(response){
+        //runFbInitCriticalCode(); 
+    });
+};
+/* End Facebook API */
